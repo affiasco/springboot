7a0583340dc64320d8238c56ae1f82bc7527deb7
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-05 21:11:20 -0500

    add qualifier

--- springcore/src/main/java/com/affiasco/springcore/rest/DemoController.java
+++ springcore/src/main/java/com/affiasco/springcore/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.affiasco.springcore.rest;
 
 import com.affiasco.springcore.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +13,13 @@ public class DemoController {
 
 //  Setter Injection
 //    @Autowired
-//    public void setCoach(Coach theCoach) {
+//    public void setCoach(@Qualifier("baseballCoach") Coach theCoach) {
 //        myCoach = theCoach;
 //    }
 
 //    Constructor Injection
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
