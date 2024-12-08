b1df3adb6c7266cbc6d1afc6a0e92bdb7b5ebf7c
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-08 16:52:16 -0500

    update app.props

--- jpa-crud/crudjpa/src/main/java/com/affiasco/crudjpa/CrudjpaApplication.java
+++ jpa-crud/crudjpa/src/main/java/com/affiasco/crudjpa/CrudjpaApplication.java
@@ -1,13 +1,24 @@
 package com.affiasco.crudjpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudjpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudjpaApplication.class, args);
+	}
+
+	@Bean
+//	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner() {
+		return runner -> {
+			System.out.println("Welcome to Crudjpa!");
+		};
 	}
 
 }
