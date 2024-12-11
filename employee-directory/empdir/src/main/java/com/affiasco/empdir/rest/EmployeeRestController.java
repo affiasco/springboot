e0bbf3a86c829a27ece679b1b4295d3692a42429
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-10 21:25:33 -0500

    add controller methods for add new employee

--- employee-directory/empdir/src/main/java/com/affiasco/empdir/rest/EmployeeRestController.java
+++ employee-directory/empdir/src/main/java/com/affiasco/empdir/rest/EmployeeRestController.java
@@ -4,7 +4,10 @@ import com.affiasco.empdir.entity.Employee;
 import com.affiasco.empdir.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,4 +27,22 @@ public class EmployeeRestController {
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee not found: " + employeeId);
+        }
+
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) { // binds the JSON requestBody to the object
+        theEmployee.setId(0); // if an id is passed in the JSON set id to 0 to force save of a new item instead of update
+        return employeeService.save(theEmployee);
+    }
+
 }
