d3da31e1c9d2354c2546f6e6c8944cdb3a775979
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-10 21:08:08 -0500

    add service methods for remaining crud operations

--- employee-directory/empdir/src/main/java/com/affiasco/empdir/service/EmployeeServiceImpl.java
+++ employee-directory/empdir/src/main/java/com/affiasco/empdir/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.affiasco.empdir.dao.EmployeeDAO;
 import com.affiasco.empdir.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,5 +21,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int employeeId) {
+        return employeeDAO.findById(employeeId);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int employeeId) {
+        employeeDAO.deleteById(employeeId);
     }
 }
