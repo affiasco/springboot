671ab1d11725eff529f1412c6d33cac49b5954ca
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-10 21:31:09 -0500

    add controller method for updating employee

--- employee-directory/empdir/src/main/java/com/affiasco/empdir/rest/EmployeeRestController.java
+++ employee-directory/empdir/src/main/java/com/affiasco/empdir/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import com.affiasco.empdir.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,8 +29,21 @@ public class EmployeeRestController {
         return employeeService.findAll();
     }
 
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) { // binds the JSON requestBody to the object
+
+        theEmployee.setId(0); // if an id is passed in the JSON set id to 0 to force save of a new item instead of update
+        return employeeService.save(theEmployee);
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        return employeeService.save(theEmployee);
+    }
+
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId) {
+
         Employee theEmployee = employeeService.findById(employeeId);
 
         if (theEmployee == null) {
@@ -37,12 +51,6 @@ public class EmployeeRestController {
         }
 
         return theEmployee;
-    }
-
-    @PostMapping("/employees")
-    public Employee addEmployee(@RequestBody Employee theEmployee) { // binds the JSON requestBody to the object
-        theEmployee.setId(0); // if an id is passed in the JSON set id to 0 to force save of a new item instead of update
-        return employeeService.save(theEmployee);
     }
 
 }
