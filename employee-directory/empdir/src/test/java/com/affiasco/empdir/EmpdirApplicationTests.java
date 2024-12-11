9770f9a8d145ca3e62c327358034da8172cc0d29
affiasco <68038507+affiasco@users.noreply.github.com>
2024-12-11 17:27:47 -0500

    Add a CRUD REST Service (#6)
    * add sql script for db
    
    * add spring init files
    
    * setup Employee entity class
    
    * create DAO and DAOImpl
    
    * add RestController, fix db table creation
    
    * add service, refactor Controller to leverage service
    
    * add daos for remaining crud methods
    
    * add service methods for remaining crud operations
    
    * add controller methods for add new employee
    
    * add controller method for updating employee
    
    * add controller methdo for deleting employee
    
    ---------
    
    Co-authored-by: Andrew Fiasco <afiasc2@wgu.edu>

---
+++ employee-directory/empdir/src/test/java/com/affiasco/empdir/EmpdirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.affiasco.empdir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpdirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
