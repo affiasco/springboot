bde636799875853c1e6fea34bc4699d5e7f264b7
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-14 11:02:15 -0500

    add data to model and use requests

--- springmvc/src/main/java/com/affiasco/mvc/controller/HelloWorldController.java
+++ springmvc/src/main/java/com/affiasco/mvc/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.affiasco.mvc.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +15,20 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormV2")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        // get the information in the "studentName" field
+        String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+        String result = "Yo! " + theName;
+
+        // "message" is name, result is value
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 }
