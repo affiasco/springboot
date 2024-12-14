b34b763deaf66e5f1a2e150a346faa07bdf615ae
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-14 12:54:22 -0500

    add spring init

---
+++ mvcvalid/src/test/java/com/affiasco/mvc/ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.affiasco.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
