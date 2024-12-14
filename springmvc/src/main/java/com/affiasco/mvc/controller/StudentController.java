540e948b42b1c4b822066343af04b5ab20217206
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-14 11:45:32 -0500

    example for data binding

---
+++ springmvc/src/main/java/com/affiasco/mvc/controller/StudentController.java
@@ -0,0 +1,31 @@
+package com.affiasco.mvc.controller;
+
+import com.affiasco.mvc.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel){
+
+        // create new student object
+        Student theStudent = new Student();
+
+        // add student object to the model
+        theModel.addAttribute("student", theStudent);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    // bind modelAttribute to param being passed in, so fills in the student object with the information
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+        return "student-confirmation";
+    }
+}
