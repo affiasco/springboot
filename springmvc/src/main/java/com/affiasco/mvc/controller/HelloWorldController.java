4760d1abdc395718aff2a92326967cfc0eb2f22d
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-14 11:13:01 -0500

    new example using RequestParam annotation

--- springmvc/src/main/java/com/affiasco/mvc/controller/HelloWorldController.java
+++ springmvc/src/main/java/com/affiasco/mvc/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -21,10 +22,23 @@ public class HelloWorldController {
     @RequestMapping("/processFormV2")
     public String letsShoutDude(HttpServletRequest request, Model model) {
 
-        // get the information in the "studentName" field
+        // get the information in the "studentName" field from the request
         String theName = request.getParameter("studentName");
         theName = theName.toUpperCase();
         String result = "Yo! " + theName;
+
+        // "message" is name, result is value
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormV3")
+    // studentName is HTML form name, bind it to theName
+    public String processForm3(@RequestParam("studentName") String theName, Model model) {
+
+        theName = theName.toUpperCase();
+        String result = "Hey there " + theName + " from v3";
 
         // "message" is name, result is value
         model.addAttribute("message", result);
