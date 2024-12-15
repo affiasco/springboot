276a2070060c0238abd8a32b4f498b4a8d1c2434
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-15 15:21:55 -0500

    add new project files

---
+++ employee-site/src/test/java/com/affiasco/empsite/EmpsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.affiasco.empsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
