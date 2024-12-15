10554873be1646d795d1d34137f5965dedab5d4c
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-14 20:25:56 -0500

    changes for adding an employee

--- mvccrud/src/main/java/com/affiasco/springboot/mvccrud/dao/EmployeeRepository.java
+++ mvccrud/src/main/java/com/affiasco/springboot/mvccrud/dao/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.affiasco.springboot.mvccrud.dao;
 import com.affiasco.springboot.mvccrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
