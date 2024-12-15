a6a5d0f88e9e8066e7d9b93dd84ed473245073e2
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-15 16:37:59 -0500

    add custom security login to check for authentication

--- employee-site/src/main/java/com/affiasco/empsite/security/SecurityConfig.java
+++ employee-site/src/main/java/com/affiasco/empsite/security/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.affiasco.empsite.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -18,5 +20,20 @@ public class SecurityConfig {
         UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // this checks that every request coming in must be authenticated
+        http.authorizeHttpRequests(configurer ->
+                        configurer.anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/showMyLoginPage")              // gives the route to login page (set in the controller)
+                        .loginProcessingUrl("/authenticateTheUser") // route for the login form to POST data to (to check un/pw), no controller required
+                        .permitAll()                                // everyone can access the login page even without logging in
+                );
+
+        return http.build();
     }
 }
