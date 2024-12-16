b0475acae72d6632c7a87eba3485b8305782edeb
affiasco <68038507+affiasco@users.noreply.github.com>
2024-12-16 18:22:16 -0500

    MVC Security (#15)
    * add new project files
    
    * add gitignore
    
    * add base controller and homepage
    
    * create temp in-memory users
    
    * add custom security login to check for authentication
    
    * add controller and custom login page
    
    * handle error in ui
    
    * add fancy bootstrap login form
    
    * add logout functionality and messaging
    
    * show user and role on homepage
    
    * setup request matchers based on roles, and add /leaders page
    
    * add notes to securityconfig
    
    * add /systems page with restricted access
    
    * add access-denied routing and page based on roles
    
    * restrict content based on roles
    
    * add sql-scripts and create db
    
    * add mysql and jdbc dependencies, update app properties
    
    * update for getting users/roles from mysql
    
    * update to set custom tables and get roles/users from there
    
    * add security configuration for new registration users
    
    * add registration form
    
    * remove register code
    
    ---------
    
    Co-authored-by: Andrew Fiasco <afiasc2@wgu.edu>

---
+++ employee-site/src/main/java/com/affiasco/empsite/security/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.affiasco.empsite.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) { // datasource injected and autoconfigured by spring
+        JdbcUserDetailsManager udm = new JdbcUserDetailsManager(dataSource);
+
+        // ? is param sent by login form
+        udm.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+        udm.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
+
+        return udm;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationSuccessHandler customAuthenticationSuccessHandler)
+            throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                // gives permissions to view certain routes based on role
+                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                .requestMatchers("/systems/**").hasRole("ADMIN")
+                                // this checks that every request coming in must be authenticated
+                                .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/showMyLoginPage")                      // gives the route to login page (set in the controller)
+                        .loginProcessingUrl("/authenticateTheUser")         // route for the login form to POST data to (to check un/pw), no controller required
+                        .successHandler(customAuthenticationSuccessHandler) // tells spring to execute the customAuthHandler for each successful login
+                        .permitAll()                                        // everyone can access the login page even without logging in
+                )
+                .logout(logout -> logout.permitAll() // sets up /logout route and for everyone to see it
+                )
+                // when access is denied to a role this is the returned view
+                .exceptionHandling(configurer -> configurer.accessDeniedPage("/access-denied")
+                );
+
+
+        return http.build();
+    }
+
+    // in memory auth, does not user application.properties information
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+//
+//        UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
+//        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
+//        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+//
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
+}
