be26ce9c0080fc86649f20adc4c49badd444105f
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-16 17:34:16 -0500

    update for getting users/roles from mysql

--- employee-site/src/main/java/com/affiasco/empsite/security/SecurityConfig.java
+++ employee-site/src/main/java/com/affiasco/empsite/security/SecurityConfig.java
@@ -3,29 +3,22 @@ package com.affiasco.empsite.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
-    // in memory auth, does not user application.properties information
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-
-        UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
-        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
-        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) { // datasource injected and autoconfigured by spring
+            return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-
         http.authorizeHttpRequests(configurer ->
                         configurer
                                 // gives permissions to view certain routes based on role
@@ -48,4 +41,15 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    // in memory auth, does not user application.properties information
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+//
+//        UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
+//        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
+//        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+//
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
 }
