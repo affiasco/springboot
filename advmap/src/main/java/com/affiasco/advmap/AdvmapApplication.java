ea7cb68c1ae49b5d6a51f3c927c718e39deb9f30
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-17 16:06:10 -0500

    set app properties, write entities, set mapping

--- advmap/src/main/java/com/affiasco/advmap/AdvmapApplication.java
+++ advmap/src/main/java/com/affiasco/advmap/AdvmapApplication.java
@@ -1,13 +1,23 @@
 package com.affiasco.advmap;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AdvmapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdvmapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdvmapApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner clr() {
+
+        return runner -> {
+            System.out.println("Hello from runner");
+        };
+    }
 
 }
