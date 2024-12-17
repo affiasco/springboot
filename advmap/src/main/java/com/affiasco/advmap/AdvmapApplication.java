58ac5c0092227b25bede0635e26022de8fbee7cd
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-17 15:48:01 -0500

    generate project

---
+++ advmap/src/main/java/com/affiasco/advmap/AdvmapApplication.java
@@ -0,0 +1,13 @@
+package com.affiasco.advmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvmapApplication.class, args);
+	}
+
+}
