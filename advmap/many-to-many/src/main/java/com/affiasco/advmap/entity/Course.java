ae962ad9a12d13832d4b8cac5d2605e9c8813974
Andrew Fiasco <afiasc2@wgu.edu>
2024-12-18 11:51:04 -0500

    set up manytomany with course and student

--- advmap/many-to-many/src/main/java/com/affiasco/advmap/entity/Course.java
+++ advmap/many-to-many/src/main/java/com/affiasco/advmap/entity/Course.java
@@ -26,6 +26,14 @@ public class Course {
     @JoinColumn(name = "course_id")
     private List<Review> reviews;
 
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "course_id"), // refers to the course table
+            inverseJoinColumns = @JoinColumn(name = "student_id")) // refers to the student table
+    private List<Student> students;
+
     public Course() {
     }
 
@@ -65,13 +73,29 @@ public class Course {
         this.reviews = reviews;
     }
 
-    // convenience method
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    // convenience methods
     public void addReview(Review theReview) {
         if (reviews == null) {
             reviews = new ArrayList<>();
         }
 
         reviews.add(theReview);
+    }
+
+    public void addStudent(Student theStudent) {
+        if (students == null) {
+            students = new ArrayList<>();
+        }
+
+        students.add(theStudent);
     }
 
     @Override
